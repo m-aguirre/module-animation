89ad14b04463780f12cdcab59261f36a8e8f597a
Matthew Aguirre <c_matthew.aguirre@lpa-mmor-mbpr.corp.light.co>
2018-08-24 11:14:07 -0700

    adds contianer component

---
+++ src/components/Container.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+class Container extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <div>Hey</div>
+      </div>
+
+    )
+  }
+}
+
+export default Container;
